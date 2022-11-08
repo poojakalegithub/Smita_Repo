fb698cdc0a1d6ce2211167cde9e16bfddefc043a
SmitaPradhan2299 <Smita.Pradhan@emtecinc.com>
2022-11-08 14:37:17 +0530

    CSharp Initial Commit

---
+++ PracticeCSharp/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace PracticeCSharp
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            //variable declaration
+            int num1 = 10;
+            int num2 = 20;
+            
+            //Creating object of vardemo class which is in PracticeCode folder
+            PracticeCode.vardemo var=new PracticeCode.vardemo();
+
+            Console.WriteLine("Mathematical Operations");
+            //calling functions
+            int add=var.Add( num1,num2);
+            Console.WriteLine("Addition is:"+add);
+            int substract=var.Substract(num1,num2);
+            Console.WriteLine("Substraction is:"+substract);
+            int multiply=var.Multiply(num1,num2);
+            Console.WriteLine("Multiplication is:"+multiply);
+            int divide=var.Divide(num1,num2);
+            Console.WriteLine("Division is:"+divide);
+
+            Console.WriteLine("_______________________________________________________");
+
+            PracticeCode.ifelsedemo ifelseobj = new PracticeCode.ifelsedemo();
+            ifelseobj.FindGreaterNumber();
+
+            Console.WriteLine("_______________________________________________________");
+
+            PracticeCode.forloop forObj = new PracticeCode.forloop();
+            forObj.printTable();
+
+            Console.WriteLine("_______________________________________________________");
+
+            Assignments.StringOperations StringObj=new Assignments.StringOperations();
+            StringObj.concatStrings();
+
+            Console.WriteLine("_______________________________________________________");
+            StringObj.compareStrings();
+
+            Console.WriteLine("_______________________________________________________");
+            StringObj.findSubString();
+
+            Console.WriteLine("_______________________________________________________");
+            StringObj.reverse();
+        }
+
+
+    }
+}
